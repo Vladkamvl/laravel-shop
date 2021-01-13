91ff6c195a9c59394c1c4b0b73557880f472e8c7
vladkamvl <vladkamvl@yandex.ru>
2021-01-13 13:56:11 +0500

    Rename models and migrations

--- database/migrations/2021_01_12_184159_create_shop_categories_table.php
+++ database/migrations/2021_01_12_184159_create_shop_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateShopCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('shop_categories', function (Blueprint $table) {
             $table->id();
 
             $table->string('title')->unique();
             $table->string('slug')->unique();
 
-            $table->text('image');
+            $table->text('image')->nullable();
             $table->string('description')->nullable();
 
 
--- database/migrations/2021_01_12_184215_create_shop_products_table.php
+++ database/migrations/2021_01_12_184215_create_shop_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateShopProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('shop_products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id');
 
@@ -25,7 +25,7 @@ class CreateProductsTable extends Migration
 
             $table->double('price')->default(-1);
 
-            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('category_id')->references('id')->on('shop_categories');
 
             $table->timestamps();
         });
